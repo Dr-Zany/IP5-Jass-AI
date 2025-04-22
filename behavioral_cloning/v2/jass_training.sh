8646c2f069de88f1b8c861048f0846b97099f145
remo.aebi (s) <remotim@hotmail.ch>
2025-04-22 11:26:22 +0200

    fix gpu issue

--- behavioral_cloning/v2/jass_training.sh
+++ behavioral_cloning/v2/jass_training.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -l
 #SBATCH -p performance
-#SBATCH -t 10:00:00
+#SBATCH -t 15:00:00
 #SBATCH --mem=50g
-#SBATCH --cpus-per-task=4
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=2
 #SBATCH --job-name=JassTraining
 singularity exec jass_training.sif python /home2/remo.aebi/Training_Scripts/IP5-Jass-AI/behavioral_cloning/v2/jass_v2.py
